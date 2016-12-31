049a1606827922c9c0bbef78057d7c700e33d111
JOHN LEE <nasky987@gmail.com>
2016-12-31 19:33:18 +0900

    # 스프링부트 시작하기 -  사용자 정의 보안 구성하기
    - String 타입의 reader변수 대신  Reader타입의 매개변수를 추가
    - 뷰에서 로그인 사용자 ㅈ어보를 얻을 수 있게 reader 속성으로 Reader 객체를 모델에 추가한다.

--- src/main/java/readinglist/ReadingListController.java
+++ src/main/java/readinglist/ReadingListController.java
@@ -15,8 +15,6 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class ReadingListController {
-    private static final String reader = "craig";
-
     private ReadingListRepository readingListRepository;
 
     @Autowired
@@ -25,18 +23,19 @@ public class ReadingListController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String readersBooks(Model model) {
+    public String readersBooks(Reader reader, Model model) {
         List<Book> readingList = readingListRepository.findByReader(reader);
 
         if(readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
         }
 
         return "readingList";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String addToReadingList(Book book) {
+    public String addToReadingList(Reader reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
 
