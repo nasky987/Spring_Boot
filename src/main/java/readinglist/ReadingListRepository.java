659f367bef9e79a2a6a3c831c3a006459789ab5a
JOHN LEE <nasky987@gmail.com>
2016-12-31 18:52:39 +0900

    # 스프링부트 시작하기 -  사용자 정의 보안 구성하기
    - 독자를 표현하는 타입을 기존 String에서 Reader로 수정하였다.

--- src/main/java/readinglist/ReadingListRepository.java
+++ src/main/java/readinglist/ReadingListRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
  * Created by hreeman on 12/31/16.
  */
 public interface ReadingListRepository extends JpaRepository<Book, Long> {
-    List<Book> findByReader(String reader);
+    List<Book> findByReader(Reader reader);
 }
