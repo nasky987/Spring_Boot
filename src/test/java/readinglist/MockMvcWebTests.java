2bdd07d964804c53b5da08cd86db2274b0acd5aa
JOHN LEE <nasky987@gmail.com>
2017-01-12 17:55:45 +0900

    # 스프링부트 시작하기 -  테스트하기
    - 생성한 MockMvc를 이용하여 간단한 테스트코드를 작성한다.
    - 작성된 코드는 /로  HTTP GET 요청을 수행하고, 요청이 성공하면(200 코드를 검증) 응답 뷰의 논리적인 이름이 readingList이기를 기대한다.
    또한 모델에 books속성을 포함하고, 이 속성이 빈 컬렉션임을 검증한다.
    - 테스트는 gradle test명령을 이용하여 테스트 해볼 수 있다.
    - 이때, 뷰 템플릿의 ${reader.fullname}이 존재하지 않으므로 에러가 발생하므로 ${‘’}식으로 지워서 테스트해야 한다.

--- src/test/java/readinglist/MockMvcWebTests.java
+++ src/test/java/readinglist/MockMvcWebTests.java
@@ -1,12 +1,16 @@
 package readinglist;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -27,5 +31,14 @@ public class MockMvcWebTests {
         mockMvc = MockMvcBuilders
                     .webAppContextSetup(webApplicationContext)
                     .build();
+    }
+
+    @Test
+    public void homePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("readingList"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("books"))
+                .andExpect(MockMvcResultMatchers.model().attribute("books", Matchers.is(Matchers.empty())));
     }
 }
