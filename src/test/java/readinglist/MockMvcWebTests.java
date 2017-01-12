35dc638586ca696f993cd9b0627568d9ababbd12
JOHN LEE <nasky987@gmail.com>
2017-01-12 18:17:47 +0900

    # 스프링부트 시작하기 -  테스트하기
    - 내부적으로 테스트 가 2개가 들어가 있는데, 첫번째 부분은 책을 등록하고, 요청 결과를 검증한다.
    - 두번째 부분은 메인 페이지에 새로운 GET요청을 실행하여, 새로 생성된 책이 모델에 있는지 검증한다.
    - 책 정보를 등록할때는 웹 브라우저가 애플리케이션에 전송할때 사용하는 application/x-www-form-urlencoded(MediaType.APPLICATION_FORM_URLENCODED)로 콘텐츠 타입을 설정해야 한다.
    - param메소드를 통해 전송할 폼을 시뮬레이션하는 필드들을 설정한다.
    - 요청을 실행하면 응답이 /로 리다이렉트되는지 검증한다.
    - 이후에 응답을 기대하는 값을 담은 Book객체를 생성하고, 메인페이지를 요청하고 반환된 모델 값과 비교할때 사용한다.
    - 여기까지가 이전에 보안 설정을 적용하기 전에 보안이 없는 애플리케이션 테스트를 한다고 가정하고 진행된 것으로, 이 다음부터는 보안을 적용한 애플리케이션 테스트 방법에 대해 확인한다.

--- src/test/java/readinglist/MockMvcWebTests.java
+++ src/test/java/readinglist/MockMvcWebTests.java
@@ -5,15 +5,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
@@ -42,5 +43,37 @@ public class MockMvcWebTests {
                 .andExpect(view().name("readingList"))
                 .andExpect(model().attributeExists("books"))
                 .andExpect(model().attribute("books", is(empty())));
+    }
+
+    @Test
+    public void postBook() throws Exception {
+        mockMvc.perform(post("/")
+                            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                            .param("title", "BOOK TITLE")
+                            .param("author", "BOOK AUTHOR")
+                            .param("isbn", "1234567890")
+                            .param("description", "DESCRIPTION"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "/"));
+
+//        Reader reader = new Reader();
+//        reader.setFullname("JOHN LEE");
+//        reader.setUsername("hreeman");
+//        reader.setPassword("1234");
+
+        Book expectedBook = new Book();
+        expectedBook.setId(1L);
+//        expectedBook.setReader(reader);
+        expectedBook.setTitle("BOOK TITLE");
+        expectedBook.setAuthor("BOOK AUTHOR");
+        expectedBook.setIsbn("1234567890");
+        expectedBook.setDescription("DESCRIPTION");
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", hasSize(1)))
+                .andExpect(model().attribute("books", contains(samePropertyValuesAs(expectedBook))));
     }
 }
