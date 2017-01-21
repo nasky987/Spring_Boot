ca529e1c664e95f2932b6a86e539d2c8bd140df4
JOHN LEE <nasky987@gmail.com>
2017-01-21 17:18:19 +0900

    # 스프링부트 시작하기 -  실행중인 애플리케이션 테스트하기(임의의 포트 사용)
    - 서버의 응답이 HTTP 404로 오는지 확인하기 위해 스프링 RestTemplate를 사용
    - 임의의 포트로 서버를 실행하기 위해서는 @WebIntegrationTest(value={“server.port=0”})과 같이 value속성에 배열을 넣어 사용하거나,
    @WebIntegrationTest(randomPort=true)를 사용하도록 한다.
    - 그러나 스프링부트 1.4에서는 지원하지 않으므로, WebEnvironment.RANDOM_PORT를 사용한다.
    - 스프링 부트에서는 임의의 포트값을 local.server.port 라는 프로퍼티에 설정을 해준다.
    - 따라서 스프링의 @Value애너테이션을 사용하여 이 프로퍼티값을 인스턴스 변수에 주입하면 된다. @Value(“${local.server.port}”)
    - RestTemplate의 getForObject메서드를 호출 할 때, 마지막 매개변수로 port값을 전달하며, 플레이스홀더로 {port}를 지정하여,
    해당 플레이스홀더에 값이 들어가도록 설정한다.

--- src/test/java/readinglist/SimpleWebTest.java
+++ src/test/java/readinglist/SimpleWebTest.java
@@ -2,6 +2,7 @@ package readinglist;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,17 +17,20 @@ import static org.junit.Assert.fail;
  */
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SimpleWebTest {
+    @Value("${local.server.port}")
+    private int port;
+
     @Test(expected = HttpClientErrorException.class)
     public void pageNotFound() {
-     try {
-         RestTemplate restTemplate = new RestTemplate();
-         restTemplate.getForObject("http://localhost:8080/bogusPage", String.class);
-         fail("Should result in HTTP 404");
-     } catch (HttpClientErrorException exception) {
-         assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
-         throw exception;
-     }
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getForObject("http://localhost:{port}/bogusPage", String.class, port);
+            fail("Should result in HTTP 404");
+        } catch (HttpClientErrorException exception) {
+            assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+            throw exception;
+        }
     }
 }
