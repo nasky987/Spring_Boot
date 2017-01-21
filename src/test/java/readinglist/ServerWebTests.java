9e5b60210f072d56ecae81aa91aaf7c7eec4c7bf
JOHN LEE <nasky987@gmail.com>
2017-01-21 18:30:59 +0900

    # 스프링부트 시작하기 -  실행중인 애플리케이션 테스트하기(셀레늄으로 HTML 페이지 테스트하기)
    - 책의 예제가 2장 소스 기준인데, 진행중인 소스는 4장이므로, 스프링시큐리티에 의해 제대로된 테스트하기엔 이미 멀리왔다….
    - 우선 div대신 css셀렉터를 이용하여 div밑에  p 태그의 내용을 가져오도록 하였으며
    스프링시큐리티의 간단인증만 하도록하여, 최초의 로그인만 수동으로 진행하여 테스트하였다.
    - 테스트시 폼에 데이터를 넣어주는것과, 폼의 submit을 호출하는것 까지 확인하였으며
    스프링시큐리티 설정 문제로 메인페이지로 돌아오는 부분은 확인하지 못 하고 우선 종료 나중에 제대로된 테스트를 만들어보자
    - 또한 브라우저별로 설치하거나 설정해야하는 부분이 존재하는듯 하다.
    - 사파리의 경우 http://www.seleniumhq.org/download/ 에서 사파리 확장기능을 다운받아 설치해야한다.
    앞으로 다룰 내용은 그루비를 이용한 부분인 듯 하다.

--- src/test/java/readinglist/ServerWebTests.java
+++ src/test/java/readinglist/ServerWebTests.java
@@ -2,13 +2,17 @@ package readinglist;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by hreeman on 1/21/17.
@@ -31,5 +35,26 @@ public class ServerWebTests {
     @AfterClass
     public static void closeBrowser() {
         safariBrowser.quit(); //브라우저 종료
+    }
+
+    @Test
+    public void addBookToEmptyList() {
+        String baseUrl = "http://localhost:" + port;
+
+        safariBrowser.get(baseUrl); //메인페이지 조회
+
+        assertEquals("You have no books in your book list", safariBrowser.findElementByCssSelector("div > p").getText()); //빈 책 여부 검증(해당 문구 있는지 검사)
+
+        safariBrowser.findElementByName("title").sendKeys("BOOK TITLE");
+        safariBrowser.findElementByName("author").sendKeys("BOOK AUTHOR");
+        safariBrowser.findElementByName("isbn").sendKeys("1234567890");
+        safariBrowser.findElementByName("description").sendKeys("DESCRIPTION");
+        safariBrowser.findElementByTagName("form").submit(); //폼에 데이터를 추가하고 전송
+
+        WebElement dl = safariBrowser.findElementByCssSelector("dt.bookHeadLine");
+        assertEquals("BOOK TITLE by BOOK AUTHOR (ISBN: 1234567890", dl.getText());
+
+        WebElement dt = safariBrowser.findElementByCssSelector("dd.bookDescription");
+        assertEquals("DESCRIPTION", dt.getText()); //목록에 새 책이 추가되었는지 검증
     }
 }
