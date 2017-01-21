901d6f66fb0b74ebd8489f13418804fa19a657ff
JOHN LEE <nasky987@gmail.com>
2017-01-21 16:36:47 +0900

    # 스프링부트 시작하기 -  웹 보안 테스트하기
    - 테스트 메소드가 작동하는 동안 시큐리티 컨텍스트에 hreeman사용자를 로드하도록 @WithUserDetails어노테이션을 사용하용했다.
    - GET요청이후 반환되는 뷰이름과, 모델 속성의 내용을 검증하도록 했다.
    - 아직은 정상 동작을 하지 않을 것이다.

--- src/test/java/readinglist/MockMvcWebTests.java
+++ src/test/java/readinglist/MockMvcWebTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,10 +14,10 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 /**
  * Created by hreeman on 1/12/17.
@@ -81,10 +81,18 @@ public class MockMvcWebTests {
     }
 
     @Test
-    @WithMockUser(username = "hreeman", password = "1234", roles = "READER")
+    @WithUserDetails("hreeman")
     public void homePage_unauthenticatedUser() throws Exception {
+        Reader expectedReader = new Reader();
+        expectedReader.setUsername("hreeman");
+        expectedReader.setPassword("1234");
+        expectedReader.setFullname("JOHN LEE");
+
         mockMvc.perform(get("/"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(header().string("Location", "http://localhost/login"));
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attribute("reader", samePropertyValuesAs(expectedReader)))
+                .andExpect(model().attribute("books", hasSize(0)))
+                .andExpect(model().attribute("amazonID", "제휴ID"));
     }
 }
